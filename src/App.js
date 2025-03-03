3dc02372e167acc7bc40d5efcb51af5cb95b6d29
Sneha0420-svg <snehajerish@gmail.com>
2025-03-03 10:23:19 +0530

    Fixed routing issue with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import DrawerAppBar from "./components/DrawerAppBar";
 import { Home } from "./components/Home";
 import { Cart } from "./components/Cart";
 import { Products } from "./components/Products";
 import { Account } from "./components/Account";
 
-
 function App() {
   return (
-    <>
-    
-
-    <BrowserRouter>
-    <DrawerAppBar/>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/products" element={<Products/>}/>
-      <Route path="/cart" element={<Cart/>}/>
-      <Route path="/account" element={<Account/>}/>
-
-    </Routes>
-    </BrowserRouter>
-      
-    
-    </>
+    <Router>
+      <DrawerAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </Router>
   );
 }
 
